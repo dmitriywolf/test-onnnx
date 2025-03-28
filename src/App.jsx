e13521a35dddf9261546c36d45e8692cbfb4d1a3
sbwsef <dmitriyvolchenko3003@gmail.com>
2025-03-28 12:20:58 +0200

    delete bad code

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import * as ort from "onnxruntime-web";
 
 async function loadOnnxModel() {
   ort.env.wasm.numThreads = 1;
- 
+
   return await ort.InferenceSession.create(
     "https://dmitriywolf.github.io/test-onnnx/models/detector_documents_leyolo_n.onnx",
     { executionProviders: ["wasm"] }
   );
 }
 
-async function runInference(session, tensor) {
-  const results = await session.run({ images: tensor });
-  for (const key in results) delete results[key];
-}
-
 function App() {
   const sessionRef = useRef(null);
+
   const dummyDataRef = useRef(new Float32Array(1 * 3 * 320 * 320));
+
   const cancelledRef = useRef(false);
   const animationFrameRef = useRef(null);
-  const inferenceCountRef = useRef(0);
-  const lastDOMUpdateRef = useRef(0);
-  const textRef = useRef(null);
+
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     cancelledRef.current = false;
@@ -36,24 +32,18 @@ function App() {
         [1, 3, 320, 320]
       );
 
-      await runInference(sessionRef.current, tensor);
+      await sessionRef.current.run({ images: tensor });
 
-      inferenceCountRef.current += 1;
-
-      const now = performance.now();
-      // DOM обновляется максимум раз в секунду
-      if (now - lastDOMUpdateRef.current > 1000) {
-        textRef.current.textContent = `Inferences: ${inferenceCountRef.current}`;
-        lastDOMUpdateRef.current = now;
-      }
+      setCount((p) => p + 1);
 
       animationFrameRef.current = requestAnimationFrame(loop);
     }
 
     async function init() {
-      textRef.current.textContent = "Loading...";
       sessionRef.current = await loadOnnxModel();
+
       if (!sessionRef.current || cancelledRef.current) return;
+
       loop();
     }
 
@@ -67,7 +57,7 @@ function App() {
     };
   }, []);
 
-  return <p ref={textRef}>Loading...</p>;
+  return <p>Inference # {count}</p>;
 }
 
 export default App;
